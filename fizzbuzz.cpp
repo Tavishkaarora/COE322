c9a923df130c1b550fa36a5fadcb9de76019aaaf
tavishka <tavishka@isp03.tacc.utexas.edu>
2024-09-04 22:40:52 -0500

    Add COE322 file and update fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std
+using std::cout
+using std::endl
+
+int main(){
+  cout << "Hello World \n";
+  return 0
+}
+
+// C-x C-s : save this file
+//C-x C-c: quit the editor
+// C-z: suspend the editor
+// on the Unix level: fg enter brings it back
--- hw2/fizzbuzz.cpp
+++ hw2/fizzbuzz.cpp
@@ -1,19 +1,33 @@
+
+// name: Tavishka Arora UTEID: ta24435 tacc user: tavishka
 #include <iostream>
 
-int main() { 
-int x; 
-std::cout << "Enter a number: "; 
-std::cin >> x;
- 
-if (x % 3 == 0 && x % 5 == 0) { 
-std::cout << "FizzBuzz\n"; 
+//function to read user input number
+int read_number() {
+  int x;
+  std::cout << "Enter a number: ";
+  //stores number in variable x
+  std::cin >> x;
+  return x;
 }
 
-else if (x % 3 == 0) {
- 	std::cout << "Fizz\n"; 
-} 
-else if (x % 5 == 0) { 
-std::cout << "Buzz\n"; 
-}
-return 0;
+int main() {
+  //read the number from user input
+  int xNum = read_number();
+
+  //determine FizzBuzz, Fizz, or buzz, based on entered number
+  //divisible by 3 & 5
+  if (xNum % 3 == 0 && xNum % 5 == 0) {
+    std::cout << "FizzBuzz\n";
+  }
+  //divisible by only 3
+  else if (xNum % 3 == 0) {
+    std::cout << "Fizz\n";
+  }
+  //divisible by only 5
+  else if (xNum % 5 == 0) {
+    std::cout << "Buzz\n";
+  }
+  //return 0 to end main
+  return 0;
  }
