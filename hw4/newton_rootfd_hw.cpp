4935b5141a165afc5512d7fa01ace298983443ca
tavishka <tavishka@isp03.tacc.utexas.edu>
2024-09-11 10:01:37 -0500

    Adding exercise 5 from lambda functions lecture for HW4 assignment

---
+++ hw4/newton_rootfd_hw.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <functional>
+#include <cmath>
+
+//using constant doubles for the following so the value cannot later be changed as these r inherent aspects of the method
+const double h = 1e-6;
+//set error tolerance for root
+const double tolerance = 1e-10;
+//just in case there is an infinite loop
+const int max_it = 1000;
+
+//Newton method root finder
+double newton_root(std::function<double(double)> f) {
+    //lambda function for the derivativeusing finite difference
+    auto derivF = [&](double x) {
+        return (f(x + h) - f(x)) / h;
+    };
+
+    //sets starting guess for the root
+    double x0 = 1.0;
+    //loop for newton method usinf f-d
+    for (int i = 0; i < max_it; ++i) {
+        double fx = f(x0);
+        double dfx = derivF(x0);
+
+        //just in case there is division by zero if the derivative is too small
+        if (std::abs(dfx) < tolerance) {
+            break;
+        }
+        //applies Newton Method
+        double x1 = x0 - fx / dfx;
+
+        //checks if the answer is within the error tolerance
+        if (std::abs(x1 - x0) < tolerance) {
+            return x1;
+        }
+        //updates guess for next round
+        x0 = x1;
+    }
+    //gives a console error if a conclusion was not able to be reached by the method.
+    std::cerr << "Error: The maximum number of iterations was reached without convergence. This is the best possible guess: " << std::endl;
+    //returns the best guess after the maximum number of iterations
+    return x0; 
+}
+
+int main() {
+    //this is a test case for y = x^3 - 27
+    //the answer should be 3
+    auto f = [](double x) {
+        return x * x * x - 27;
+    };
+    //find the root
+    double rootAnswer = newton_root(f);
+
+    //print the results
+    std::cout << "The root, using the Newton method for roots with finite difference is: " << rootAnswer << std::endl;
+    
+    //ends the main
+    return 0;
+}
