0daade7716f4836c15a9d01c344e4fbfaa60af7a
tavishka <tavishka@isp03.tacc.utexas.edu>
2024-09-24 11:47:03 -0500

    Adjusted Unix script

--- hw5/fizzbuzz.sh
+++ hw5/fizzbuzz.sh
@@ -2,8 +2,7 @@
 
 #check if there is an input
 if [ -z "$1" ]; then
-    echo "Error: No input provided"
-    echo "Usage: $0 nnn"
+    echo "Error: Number was not entered"
     exit 1
 fi
 
@@ -15,11 +14,12 @@ if [ "$1" == "-h" ]; then
 fi
 
 #check if input is >0
-if ! [[ "$1" =~ ^[0-9]+$ ]] || [ "$1" -le 0 ]; then
-    echo "Error: Input must be a positive integer"
+if ! [[ "$1" =~ ^[1-9][0-9]*$ ]]; then
+    echo "Error: You must enter a positive integer"
     echo "Usage: $0 nnn"
     exit 1
 fi
+
 
 #run fizzbuzz program
 ./fizzbuzz "$1"
