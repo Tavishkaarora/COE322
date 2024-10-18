9724f95915b0891680c7df475119fe2d5e0580ab
tavishka <tavishka@isp03.tacc.utexas.edu>
2024-10-18 11:39:12 -0500

    added hw6 cmake

---
+++ hw6/build/CMakeFiles/perfNum.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perfNum.dir/perfNum.cpp.o"
+  "CMakeFiles/perfNum.dir/perfNum.cpp.o.d"
+  "perfNum"
+  "perfNum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perfNum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hw6/build/CMakeFiles/perfNum.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for perfNum.
+# This may be replaced when dependencies are built.
