af1595bf151bb0eb021969807986a3765c02dde0
tavishka <tavishka@isp03.tacc.utexas.edu>
2024-10-20 15:54:28 -0500

    hw9

---
+++ hw9/build/CMakeFiles/linkshared.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linkshared.dir/linkshared.cpp.o"
+  "CMakeFiles/linkshared.dir/linkshared.cpp.o.d"
+  "linkshared"
+  "linkshared.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linkshared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
